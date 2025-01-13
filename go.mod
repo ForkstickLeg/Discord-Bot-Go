6cabb7d54762b04cacb3d96f24240642cc38c470
ChopstickLeg <72234304+ChopstickLeg@users.noreply.github.com>
2025-01-12 22:35:57 -0500

    Basic webhook and websocket implementation, still need to actually do stuff with it

--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module github.com/ChopstickLeg/Discord-Bot-Practice
 go 1.23.4
 
 require github.com/joho/godotenv v1.5.1
+
+require golang.org/x/crypto v0.32.0
+
+require golang.org/x/net v0.34.0 // indirect
--- src/main.go
+++ src/main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/ed25519"
+	"golang.org/x/net/websocket"
 )
 
-type TokenResponse struct {
-	Token string `json:"access_token"`
-	URL   string `json:"url"`
+type Interaction struct {
+	Type int `json:"type"`
+}
+
+type GatewayResponse struct {
+	URL string `json:"url"`
 }
 
 type Command struct {
-	ID string `json:"id"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
-var oauthToken string
-var wslUrl string
+var botToken string
+var clientid string
+var publicKey string
+var gatewayURL string
 
 func main() {
 	err := godotenv.Load("../.env")
@@ -31,35 +42,34 @@ func main() {
 		return
 	}
 
-	clientid := os.Getenv("APP_ID")
-	clientSecret := os.Getenv("CLIENT_SECRET")
-	botToken := os.Getenv("BOT_TOKEN")
+	clientid = os.Getenv("APP_ID")
+	botToken = os.Getenv("BOT_TOKEN")
+	publicKey = os.Getenv("PUBLIC_KEY")
 
-	key := []string{"Content-Type"}
-	value := []string{"application/x-www-form-urlencoded"}
+	output := setupCommand("silence", "Use this command to totally silence someone. Specify the amount of time (in minutes), default is 1")
 
-	oauthToken = getToken(clientid, clientSecret, key, value)
+	gatewayURL = getWSUrl()
 
-	wslUrl = getWSUrl(key, value)
+	fmt.Printf("ID: %s\nName: %s\nDescription:%s", output.ID, output.Name, output.Description)
 
-	fmt.Println(wslUrl + "\n" + oauthToken)
+	http.HandleFunc("/ack", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Request acknowledged")
+	})
 
-	key = append(key, "Authorization")
-	value = append(value, "Bot "+botToken)
+	http.HandleFunc("/", postHandler)
 
-	commands := makeCall("https://discord.com/api/v10/applications/"+clientid+"/commands", "GET", key, value)
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			fmt.Println("HTTP server error", err)
+		}
+	}()
 
-	fmt.Println("Raw response:", string(commands))
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
-	var output []Command
-	err = json.Unmarshal(commands, &output)
-	if err != nil {
-		fmt.Println("Error unmarshalling response")
-	}
-
-	for _, out := range output {
-		fmt.Printf("ID: %s\n", out.ID)
-	}
+	sig := <-sigChan
+	fmt.Println("Received signal:", sig)
+	fmt.Println("Shutting down gracefully")
 }
 
 func makeCall(apiUrl string, method string, key []string, value []string, body ...string) []byte {
@@ -95,6 +105,83 @@ func makeCall(apiUrl string, method string, key []string, value []string, body .
 	return output
 }
 
+func setupWebSocket(websocketURL string, userId int) {
+	conn, err := websocket.Dial(websocketURL, "", "http://localhost/")
+	if err != nil {
+		fmt.Println("Error connecting to WebSocket:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		var message string
+		err := websocket.Message.Receive(conn, &message)
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			return
+		}
+		fmt.Printf("Received message: %s\n", message)
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		signature := r.Header.Get("X-Signature-Ed25519")
+		timestamp := r.Header.Get("X-Signature-Timestamp")
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		message := append([]byte(timestamp), body...)
+		sig, err := hex.DecodeString(signature)
+		if err != nil {
+			http.Error(w, "Invalid signature format", http.StatusBadRequest)
+			return
+		}
+
+		pubKey, err := hex.DecodeString(publicKey)
+		if err != nil {
+			http.Error(w, "Invalid public key format", http.StatusInternalServerError)
+			return
+		}
+
+		if !ed25519.Verify(pubKey, message, sig) {
+			http.Error(w, "Invalid request signature", http.StatusUnauthorized)
+			return
+		}
+
+		var interaction Interaction
+		if err := json.Unmarshal(body, &interaction); err != nil {
+			http.Error(w, "Error unmarshalling request body", http.StatusBadRequest)
+			return
+		}
+
+		if interaction.Type == 1 {
+			response := map[string]int{"type": 1}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		if interaction.Type == 2 {
+			response := map[string]interface{}{"type": 4, "data": map[string]string{"content": "User has been silenced"}}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+			return
+			//TODO: actually call the function to handle muting the person
+		}
+
+		response := map[string]int{"type": 1}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
 func cleanupAndExit(response *http.Response) {
 	if response != nil {
 		response.Body.Close()
@@ -102,32 +189,54 @@ func cleanupAndExit(response *http.Response) {
 	os.Exit(1)
 }
 
-func getToken(clientid string, clientSecret string, key []string, value []string) string {
+func setupCommand(name string, description string, commandType ...int) Command {
+	var data map[string]string
+	key := []string{"Content-Type", "Authorization"}
+	value := []string{"application/json", "Bot " + botToken}
 
-	data := url.Values{}
-	data.Set("grant_type", "client_credentials")
-	data.Set("scope", "identify connections bot")
-	data.Set("client_id", clientid)
-	data.Set("client_secret", clientSecret)
+	if len(commandType) > 0 {
+		data = map[string]string{
+			"name":        name,
+			"description": description,
+			"type":        string(rune(commandType[1])),
+		}
+	} else {
+		data = map[string]string{
+			"name":        name,
+			"description": description,
+		}
+	}
 
-	body := makeCall("https://discord.com/api/v10/oauth2/token", "POST", key, value, data.Encode())
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		os.Exit(1)
+	}
 
-	var tokenResponse TokenResponse
-	err := json.Unmarshal(body, &tokenResponse)
+	commands := makeCall("https://discord.com/api/v10/applications/"+clientid+"/commands", "POST", key, value, string(jsonData))
+
+	var output Command
+	err = json.Unmarshal(commands, &output)
 	if err != nil {
 		fmt.Println("Error unmarshalling response")
 	}
-
-	return tokenResponse.Token
+	return output
 }
 
-func getWSUrl(key []string, value []string) string {
+func getWSUrl() string {
+	key := []string{"Content-Type", "Authorization"}
+	value := []string{"application/json", "Bot " + botToken}
+
 	body := makeCall("https://discord.com/api/v10/gateway", "GET", key, value)
 
-	var tokenResponse TokenResponse
-	err := json.Unmarshal(body, &tokenResponse)
+	var output GatewayResponse
+	err := json.Unmarshal(body, &output)
 	if err != nil {
 		fmt.Println("Error unmarshalling response")
 	}
-	return tokenResponse.URL
+	return output.URL
+}
+
+func silence(memberId int, minutes int) {
+
 }
