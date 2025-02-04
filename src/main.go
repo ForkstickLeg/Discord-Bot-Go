8684bd433ca4cf0d771d2290d3b2dc196814d35c
ChopstickLeg <72234304+ChopstickLeg@users.noreply.github.com>
2025-02-03 23:04:58 -0500

    Still trying to fix deployment issues

--- src/main.go
+++ src/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ChopstickLeg/Discord-Bot-Practice/src/database"
 	"github.com/ChopstickLeg/Discord-Bot-Practice/src/discordclient"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-		return
-	}
-
 	db := database.CreateDB("discordbot")
 	defer db.Close()
 
