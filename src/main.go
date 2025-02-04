f2f3ede2e05c1fd1ef2038f70553f2804cfb0f4c
ChopstickLeg <72234304+ChopstickLeg@users.noreply.github.com>
2025-02-04 00:22:01 -0500

    More debugging yml files

--- src/main.go
+++ src/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/ChopstickLeg/Discord-Bot-Practice/src/database"
 	"github.com/ChopstickLeg/Discord-Bot-Practice/src/discordclient"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+	App_id := os.Getenv("APP_ID")
+	log.Println(App_id)
+
 	db := database.CreateDB("discordbot")
 	defer db.Close()
 
